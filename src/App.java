3eda3e48da4612863185ab0c0838fa22090e08e4
Pollyana <pollyanarocha416@gmail.com>
2022-12-12 20:24:08 -0300

    if e else e final

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args){
+        final String br = "Brasil";
         int number = 5;
         if (number < 4) {
             System.out.println("menor");
