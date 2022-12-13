e0a9932fd7d4304a9a9a0d1b7c6bbd24698986af
Pollyana <pollyanarocha416@gmail.com>
2022-12-13 19:51:40 -0300

    metodos e convencoes q seve ser utilizada

--- src/edu/polly/segundasemana/App.java
+++ src/edu/polly/segundasemana/App.java
@@ -7,12 +7,19 @@ public class App {
         String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
         System.out.println(nomeCompleto);
         
-
+        int val1 = 7;
+        int val2 = 7;
+        int soma = somar(val1, val2);
+        System.out.println(soma);
         // ctrl + k + c pra comentar varias linhas
         // ctrl + k + u pra descomentar
         
     }
+    // o metodo so pode ter uma unica funcionalidade
     public static String nomeCompleto(String primeiroNome, String segundoNome){
         return "Resul do metodo " + primeiroNome.concat(" ").concat(segundoNome);
+    }
+    public static int somar(int val1, int val2){
+        return val1 + val2;
     }
 }
