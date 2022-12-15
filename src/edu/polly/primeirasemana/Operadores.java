52abfc35aacf93294442136feba08228ea782a51
Pollyana <pollyanarocha416@gmail.com>
2022-12-15 18:20:55 -0400

    operador unario

--- src/edu/polly/primeirasemana/Operadores.java
+++ src/edu/polly/primeirasemana/Operadores.java
@@ -9,6 +9,14 @@ public class Operadores {
         // maior igual 
         String nomCompleto = "Linguagem " + "java";
         System.out.println(nomCompleto);
-        
+        // operador unario -, +, ++ incremento, -- decremento, ! negacao:
+        int num = 5;
+        // tornar o num negativo:
+        num = - num;
+        System.out.println(num);
+        // tornar o num positivo:
+        num = num * -1;
+        System.out.println(num);
+
     }
 }
