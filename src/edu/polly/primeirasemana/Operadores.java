bf4c0a80f6d09611fbc87911e2b5e9660abc50fc
Pollyana <pollyanarocha416@gmail.com>
2022-12-15 18:14:12 -0400

    operadores e concatenacao

---
+++ src/edu/polly/primeirasemana/Operadores.java
@@ -0,0 +1,14 @@
+package edu.polly.primeirasemana;
+public class Operadores {
+    public static void main(String[] args){
+        // para o char tem q ser ''
+        // operadores:
+        // atribuicao =
+        // maior >
+        //menor <
+        // maior igual 
+        String nomCompleto = "Linguagem " + "java";
+        System.out.println(nomCompleto);
+        
+    }
+}
