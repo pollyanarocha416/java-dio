2f46647cb1da7f910db65eaf2fd444a692fe00ad
Pollyana <pollyanarocha416@gmail.com>
2022-12-20 18:17:08 -0400

    documentacao como escrever

--- src/edu/polly/segundasemana/docs.java
+++ src/edu/polly/segundasemana/docs.java
@@ -1,5 +1,17 @@
 package edu.polly.segundasemana;
 
+/**
+ * <h1>docs</h1>
+ * A docs realiza operacoes matematicas entre numeros inteiros
+ * <p>
+ * <b>Note:</b> Leia atentamente a docs desta classe
+ * para desfrutar dos recursos oferecidos pelo autor
+ * 
+ * @pollyanarocha416
+ * @version 1.0
+ * @since 20/12/2022
+ */
 public class docs {
-    
+    // ser consiso na hora de criar
+
 }
