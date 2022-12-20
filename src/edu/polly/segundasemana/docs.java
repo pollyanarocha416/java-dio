95964f4aa2109e08960a5aac955037d1b90c59e0
Pollyana <pollyanarocha416@gmail.com>
2022-12-20 18:23:22 -0400

    esse e um exemplo de como nao criar uma class

--- src/edu/polly/segundasemana/docs.java
+++ src/edu/polly/segundasemana/docs.java
@@ -15,3 +15,14 @@ public class docs {
     // ser consiso na hora de criar
 
 }
+
+public int somaMultiplica (int n, int x, String m){
+    int r = 0; // r é igual ao resultado
+    if (m == "M"){ // M= multiplicação
+        r= n * x;
+    }else{
+        // se não soma mesmo
+        r = n + x;
+    }
+    return r;
+}
