553a921812cad4bd5b22a93fc7374948ffba7435
Pollyana <pollyanarocha416@gmail.com>
2022-12-19 21:18:54 -0400

    palavras reservadas, todas as 52.

---
+++ src/edu/polly/segundasemana/palavrasreservadas.java
@@ -0,0 +1,7 @@
+package edu.polly.segundasemana;
+
+public class palavrasreservadas {
+    //tipos primitivos classificacoes, escopo de uso, palavras opostas:
+    // int, double etc..
+    // palavras q estao sempre em minusculo 
+}
