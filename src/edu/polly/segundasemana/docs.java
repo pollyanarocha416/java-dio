3a9072b587cfbdbac1492a5e7ffb454e566d2034
Pollyana <pollyanarocha416@gmail.com>
2022-12-20 17:35:05 -0400

    palavras reservadas

---
+++ src/edu/polly/segundasemana/docs.java
@@ -0,0 +1,5 @@
+package edu.polly.segundasemana;
+
+public class docs {
+    
+}
--- src/edu/polly/segundasemana/palavrasreservadas.java
+++ src/edu/polly/segundasemana/palavrasreservadas.java
@@ -3,5 +3,27 @@ package edu.polly.segundasemana;
 public class palavrasreservadas {
     //tipos primitivos classificacoes, escopo de uso, palavras opostas:
     // int, double etc..
-    // palavras q estao sempre em minusculo 
+    // palavras q estao sempre em minusculo
+    public static void main(String[] args) {
+        short num4 = -1;
+        System.out.println(num4);
+    }
+    // public: qualquer outra class pode acessar o metodo
+    public String texto(){
+        return "";
+    }
+
+    // private:
+    //private double numero(){
+        //return 
+    //}
+    protected double valores(int valor, int valor2){
+        return valor + valor2;
+    }
+
+    // void indica q nao tem retorno de valor
+    // double: 34555, 3.45 mais valor q int(435, 4534, 34, etc..)
+    // float e 3.14F
+    // short: valor menor q int
+    
 }
