159351289c4b9f459e47cdb826a9d67d11bc998d
Pollyana <pollyanarocha416@gmail.com>
2022-12-22 21:03:54 -0400

    if e for em condicionais

---
+++ src/edu/polly/segundasemana/condicionais.java
@@ -0,0 +1,20 @@
+package edu.polly.segundasemana;
+
+public class condicionais {
+    public static void main(String[] args) {
+        int num = 5;
+        if (num <= 1) {
+            System.out.println("menor q 1");
+        }else {
+            System.out.println("maior q 1");
+        }
+        for (int i = 0; i < num; i++) {
+            System.out.println("i = " + i);
+        }
+    
+    
+    
+    
+    }
+    
+}
--- src/edu/polly/segundasemana/docs.java
+++ src/edu/polly/segundasemana/docs.java
@@ -16,13 +16,13 @@ public class docs {
 
 }
 
-public int somaMultiplica (int n, int x, String m){
-    int r = 0; // r é igual ao resultado
-    if (m == "M"){ // M= multiplicação
-        r= n * x;
-    }else{
-        // se não soma mesmo
-        r = n + x;
-    }
-    return r;
-}
+// public int somaMultiplica (int n, int x, String m){
+//     int r = 0; // r é igual ao resultado
+//     if (m == "M"){ // M= multiplicação
+//         r= n * x;
+//     }else{
+//         // se não soma mesmo
+//         r = n + x;
+//     }
+//     return r;
+// }
