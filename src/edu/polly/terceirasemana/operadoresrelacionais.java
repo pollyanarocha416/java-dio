a800e2526f59fcca7e233f850f53725c56934bba
Pollyana <pollyanarocha416@gmail.com>
2022-12-26 20:08:39 -0400

    operador de relacao

---
+++ src/edu/polly/terceirasemana/operadoresrelacionais.java
@@ -0,0 +1,18 @@
+package edu.polly.terceirasemana;
+
+public class operadoresrelacionais {
+    public static void main(String[] args) {
+        // operador de relacao:
+        int i1 = 10;
+        float f1 = 4.5F;
+        double d1 = 59.6d;
+        char c1 = 'x';
+        String s1 = "Fulano";
+        boolean t1 = true;
+        byte y1 = 1;
+        short h1 = 25;
+        long l1 = 1597L;
+        // nao tem como comparar tipos de dados diferente
+    }
+
+}
