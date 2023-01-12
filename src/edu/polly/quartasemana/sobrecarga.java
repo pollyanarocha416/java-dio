435a09851dac06bc349ef2b620d97e852b278321
Pollyana <pollyanarocha416@gmail.com>
2023-01-12 19:53:10 -0400

    sobrecarga conceito

---
+++ src/edu/polly/quartasemana/sobrecarga.java
@@ -0,0 +1,11 @@
+package edu.polly.quartasemana;
+
+public class sobrecarga {
+    //sobrecarga e quando vc muda o parametro do metodo
+    // mas mantem o nome do metodo
+    //
+    
+    public static void main(String[] args) {
+        
+    }
+}
