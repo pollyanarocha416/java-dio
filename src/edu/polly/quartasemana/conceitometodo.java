023acbf360a3031447b50f7689a7a9aa7b9ad6aa
Pollyana <pollyanarocha416@gmail.com>
2023-01-11 20:30:10 -0400

    mais sobre metodos void,static etc

--- src/edu/polly/quartasemana/conceitometodo.java
+++ src/edu/polly/quartasemana/conceitometodo.java
@@ -7,13 +7,36 @@ package edu.polly.quartasemana;
 public class conceitometodo {
     public static void main(String[] args) {
         conceitometodo.fazer(5);
-
+        conceitometodo.somar(5, 6);
+        conceitometodo.subtrair(5, 6);
+        conceitometodo.multiplicar(5, 6);
+        conceitometodo.dividir(5, 6);
     }
         // metodo publico do tipo string 
    
     public static void fazer(int i){
         i = i + 10;
         System.out.println("val de i: " + i);
+    }
+    public static int somar(int a, int b){
+        return a + b;
+    }
+    
+    public static int subtrair(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiplicar(int a, int b) {
+        return a * b;
+    }
+
+    public static int dividir(int a, int b) {
+        return a / b;
+    }
+
+    public static void manhan(String saldacao){
+        saldacao = "bom dia";
+        System.out.println(saldacao);
     }
     
 }
