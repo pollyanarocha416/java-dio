644d60c5e4f34c2e19149fb202e175d0523ae7ee
Pollyana <pollyanarocha416@gmail.com>
2023-01-11 20:12:40 -0400

    mais conceitos sobre metodos

--- src/edu/polly/quartasemana/conceitometodo.java
+++ src/edu/polly/quartasemana/conceitometodo.java
@@ -1,3 +1,19 @@
 package edu.polly.quartasemana;
 
 // tudo q e execultado vai ser nos metodos
+
+
+// metodo principal
+public class conceitometodo {
+    public static void main(String[] args) {
+        conceitometodo.fazer(5);
+
+    }
+        // metodo publico do tipo string 
+   
+    public static void fazer(int i){
+        i = i + 10;
+        System.out.println("val de i: " + i);
+    }
+    
+}
