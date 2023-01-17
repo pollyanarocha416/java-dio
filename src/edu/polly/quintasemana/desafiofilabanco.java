f6acc14c65efd016fae041529c3817d01bef2ed3
Pollyana <pollyanarocha416@gmail.com>
2023-01-17 19:14:50 -0400

    corrigido

--- src/edu/polly/quintasemana/desafiofilabanco.java
+++ src/edu/polly/quintasemana/desafiofilabanco.java
@@ -4,14 +4,15 @@ import java.util.Scanner;
 
 public class desafiofilabanco {
     public static void main(String[] args) {
-    String[] nomesFila = new String[3]; 
-    Scanner nome = new Scanner(System.in); 
 
-    if(nomesFila == "Ana"){
-      System.out.println(nome +" - esta na posicao: 1");
-    }else if(nomesFila == "Sofia")
-      System.out.println("{} - esta na posicao: 2");
-    else{
-      System.out.println("Lucas - esta na posicao: 3");
+        String[] nomesFila = new String[3];
+        Scanner nome = new Scanner(System.in);
+
+        // posição do cliente na fila:
+        for (int i = 0; i < nomesFila.length; i++) {
+            nomesFila[i] = nome.nextLine();
+            System.out.println(nomesFila[i] + " - esta na posicao: " + (i + 1));
+        }
+        nome.close();
     }
 }
