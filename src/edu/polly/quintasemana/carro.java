92d8d5e28e744ac5a207cf49928dd38fe9102bd6
Pollyana <pollyanarocha416@gmail.com>
2023-01-19 19:48:53 -0400

    atributos e como declarar

--- src/edu/polly/quintasemana/carro.java
+++ src/edu/polly/quintasemana/carro.java
@@ -1,5 +1,10 @@
 package edu.polly.quintasemana;
 
 public class carro {
-    
+    // atributos de carro:
+    // cor, peneu, vidraca, parabrisa...
+    int portas;
+    String cor;
+    String modelo;
+    int capcidadeTanque;
 }
