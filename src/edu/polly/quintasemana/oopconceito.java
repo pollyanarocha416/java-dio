4907432384f82c54285aad147dbd317f898c6d1d
Pollyana <pollyanarocha416@gmail.com>
2023-01-18 20:31:29 -0400

    o que vou saber

--- src/edu/polly/quintasemana/oopconceito.java
+++ src/edu/polly/quintasemana/oopconceito.java
@@ -2,6 +2,9 @@ package edu.polly.quintasemana;
 public class oopconceito {
     public static void main(String[] args) {
         // conceitos sobre OOP:
-        
+        // pq usar?
+        // os fundamentos?
+        // A organizacao
+        //prox passo
     }
 }
