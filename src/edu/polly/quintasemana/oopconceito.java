742ee61428cba8de5aa0b47a9f57543ac36e588f
Pollyana <pollyanarocha416@gmail.com>
2023-01-19 19:27:03 -0400

    estrutura da class

--- src/edu/polly/quintasemana/oopconceito.java
+++ src/edu/polly/quintasemana/oopconceito.java
@@ -1,10 +1,32 @@
 package edu.polly.quintasemana;
 public class oopconceito {
+
+    // livros online:
+    // class livro:
+    // seus metodos sao:
+    // livros disponiveis()
+    // livros lancados recentemente()
+    // livros ja lidos()
+    // livros nao lidos()
+    // ler livro()
+    // comprar um livro()
+
+    // do prof:
+    // capa, tema, autor, editora, assunto, qtd de pagina
+
+
+
     public static void main(String[] args) {
         // conceitos sobre OOP:
         // pq usar?
         // os fundamentos?
         // A organizacao
         //prox passo
+        int val = oopconceito.somar(4, 2);
+        System.out.println(val);
+    }
+    
+    public static int somar(int a, int b) {
+        return a + b;
     }
 }
