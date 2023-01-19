04f835b65c5c9bc99f08b59b12d2c779ac18b624
Pollyana <pollyanarocha416@gmail.com>
2023-01-19 19:51:32 -0400

    atributos

--- src/edu/polly/quintasemana/carro.java
+++ src/edu/polly/quintasemana/carro.java
@@ -3,7 +3,6 @@ package edu.polly.quintasemana;
 public class carro {
     // atributos de carro:
     // cor, peneu, vidraca, parabrisa...
-    int portas;
     String cor;
     String modelo;
     int capcidadeTanque;
