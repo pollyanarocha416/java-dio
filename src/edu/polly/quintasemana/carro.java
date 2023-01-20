dc82886fe63e1b9034eb9333b20a072160673c60
Pollyana <pollyanarocha416@gmail.com>
2023-01-20 18:47:45 -0400

    metodos em java

--- src/edu/polly/quintasemana/carro.java
+++ src/edu/polly/quintasemana/carro.java
@@ -6,4 +6,44 @@ public class carro {
     String cor;
     String modelo;
     int capcidadeTanque;
+
+    // igual o def init do py
+    // construtor
+    carro(){
+        
+    }
+    carro(String cor, String modelo, int capcidadeTanque){
+        this.cor = cor;
+        this.modelo = modelo;
+        this.capcidadeTanque = capcidadeTanque;
+    }
+    // destrutor:
+    ///void finalize(){
+
+    //}
+
+    void setCor(String cor){
+        this.cor = cor;
+    }
+    String getCor(){
+        return cor;
+    }
+
+    void setModelo(String modelo){
+        this.modelo = modelo;
+    }
+    String getModelo(){
+        return modelo;
+    }
+
+    void setCapcidadeTanque(int capcidadeTanque){
+        this.capcidadeTanque = capcidadeTanque;
+    }
+    int getcapcidadeTanque(){
+        return capcidadeTanque;
+    }
+    double totalValorTanque(double valorCombustivel){
+        return capcidadeTanque *valorCombustivel;
+    }
+
 }
