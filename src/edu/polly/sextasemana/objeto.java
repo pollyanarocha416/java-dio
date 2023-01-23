7b282c964a905a7a15add843a9699067a0fe60c6
Pollyana <pollyanarocha416@gmail.com>
2023-01-23 18:29:55 -0400

    o basico sobre objetos

---
+++ src/edu/polly/sextasemana/objeto.java
@@ -0,0 +1,19 @@
+package edu.polly.sextasemana;
+
+public class objeto {
+    public static void main(String[] args) {
+        // como criar um objeto:
+        // Carro carro = new Carro();
+        // carro mycarro = new carro("roxo", "ferrari", 20);
+
+
+        // mensagem:
+        // qdo vc chamma esse metodo
+        // exemplo: carro.metodo(); 
+        
+        //instancia X estático: atributos e métodos
+        // estado de um objeto
+        // identidade de um objeto
+        // representacao numerica de um objeto
+    }
+}
