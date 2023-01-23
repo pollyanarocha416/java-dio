5f57eadecc43404cb86c74f86e85fd0dab7f7159
Pollyana <pollyanarocha416@gmail.com>
2023-01-23 19:01:43 -0400

    identidade de objeto

--- src/edu/polly/sextasemana/objeto.java
+++ src/edu/polly/sextasemana/objeto.java
@@ -16,8 +16,19 @@ public class objeto {
         // Por exemplo, se você tem uma classe "Carro" com atributos como "marca",
         // "modelo" e "ano", o estado de um objeto "Carro" pode ser representado por
         // valores como "Toyota", "Camry" e "2022", respectivamente.
-        
-        // identidade de um objeto
+
+        // identidade de um objeto:
+        // // Comparando identidade de objetos
+        //System.out.println(i1 == i2); // false
+        //System.out.println(i1 == i3); // false
+        // Comparando objetos usando o método equals()
+        //System.out.println(i1.equals(i2)); // true
+        //System.out.println(i1.equals(i3)); // true
+//      eles sao diferentes == pq eles tem identidades dfrentes
+//      o equals verifica se eles possuem val iguais
+
+
+
         // representacao numerica de um objeto
     }
 }
