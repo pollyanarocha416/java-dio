a77b45b1d666fa2574a759913ab988fb91cb2403
Pollyana <pollyanarocha416@gmail.com>
2023-01-26 20:45:38 -0400

    polimorfismo e sobrescrita

--- src/edu/polly/sextasemana/heranca.java
+++ src/edu/polly/sextasemana/heranca.java
@@ -17,4 +17,8 @@ public class heranca {
     // upcast -> subir na herarquia e downcast -> decer
     // A a = new B();
     // B a = (B) new A(); -> nao e muito util
+
+    // Polimorfismo:
+    // a mesma acao se comportando diferente
+    //sobrescrita: sobre escreve algo em um caso
 }
