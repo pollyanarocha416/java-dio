95ae1f709629d8969534e14bc897945a05869a36
Pollyana <pollyanarocha416@gmail.com>
2023-02-01 20:40:55 -0400

    collection em java, conceito

---
+++ src/edu/polly/setimasemana/collectionsjava/oquesao.java
@@ -0,0 +1,16 @@
+package edu.polly.setimasemana.collectionsjava;
+
+public class oquesao {
+    // é um frameWork
+    // serve pra amarzenar e processar conjuntos 
+    // de dados de forma eficiente
+    // ele agrupa varios items(variavel e objetos)
+
+    // A Collection em Java é uma interface que permite trabalhar com grupos de
+    // objetos como se fossem um único objeto. Isso significa que você pode
+    // adicionar vários objetos a uma Collection, acessá-los e manipulá-los como se
+    // fossem um único objeto. Por exemplo, você pode criar uma Collection de
+    // números inteiros e adicionar vários números à coleção. Depois, pode usar
+    // métodos fornecidos pela Collection para percorrer todos os números na coleção
+    // e realizar alguma ação, como somar todos os números.
+}
