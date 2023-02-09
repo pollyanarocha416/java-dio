7f284bae01a58c2919600d6e761dae599761abd8
Pollyana <pollyanarocha416@gmail.com>
2023-02-08 20:35:02 -0400

    correcao pra amanha, sobre compareble

--- src/edu/polly/oitavasemana/collectionsjava/exercicio.java
+++ src/edu/polly/oitavasemana/collectionsjava/exercicio.java
@@ -3,6 +3,7 @@ package edu.polly.oitavasemana.collectionsjava;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class exercicio {
@@ -20,6 +21,20 @@ public class exercicio {
 
         Collections.sort(meusGatos);
         System.out.println(meusGatos);
+
+        System.out.println("Ordem idade: ");
+        Collections.sort(meusGatos, new ComparetorIdade());
+        meusGatos.sort(new ComparetorIdade());
+        System.out.println(meusGatos);
+
+        System.out.println("Ordem por cor: ");
+        Collections.sort(meusGatos, new ComparetorCor());
+        meusGatos.sort(new ComparetorCor());
+        System.out.println(meusGatos);
+
+        meusGatos.sort(new ComparetorNomeCorIdade());
+        System.out.println(meusGatos);
+
     }
 }
     
@@ -73,4 +88,40 @@ class Gato implements Comparable<Gato>{
     }
 
     
+}
+
+class ComparetorIdade implements Comparator<Gato>{
+
+    @Override
+    public int compare(Gato g1, Gato g2) {
+        return Integer.compare(g1.getIdade(), g2.getIdade());
+    }
+
+}
+
+class ComparetorCor implements Comparator<Gato> {
+
+    @Override
+    public int compare(Gato g1, Gato g2) {
+        return g1.getCor().compareToIgnoreCase(g2.getCor());
+    }
+
+}
+
+class ComparetorNomeCorIdade implements Comparator<gato>{
+
+    @Override
+    public int compare(Gato g1, Gato g2) {
+        int nome = g1.getNome().compareToIgnoreCase(g2.getNome());
+        if (nome != 0) {
+            return nome;
+        }
+
+        int cor = g1.getCor().compareToIgnoreCase(g2.getCor());
+        if (cor != 0) {
+            return cor;
+        }
+        return Integer.compare(g1.getIdade(), g2.getIdade());
+    }
+
 }
