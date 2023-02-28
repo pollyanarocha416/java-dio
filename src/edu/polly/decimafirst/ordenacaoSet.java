c33784230c7735880c40abf25c4a491f13f41171
Pollyana <pollyanarocha416@gmail.com>
2023-02-27 21:03:20 -0400

    uma tentativa...

--- src/edu/polly/decimafirst/ordenacaoSet.java
+++ src/edu/polly/decimafirst/ordenacaoSet.java
@@ -27,4 +27,39 @@ class Serie{
         this.genero = genero;
         this.tempoEpisodio = tempoEpisodio;
     }
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        result = prime * result + ((genero == null) ? 0 : genero.hashCode());
+        result = prime * result + ((tempoEpisodio == null) ? 0 : tempoEpisodio.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Serie other = (Serie) obj;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        if (genero == null) {
+            if (other.genero != null)
+                return false;
+        } else if (!genero.equals(other.genero))
+            return false;
+        if (tempoEpisodio == null) {
+            if (other.tempoEpisodio != null)
+                return false;
+        } else if (!tempoEpisodio.equals(other.tempoEpisodio))
+            return false;
+        return true;
+    }
 }
